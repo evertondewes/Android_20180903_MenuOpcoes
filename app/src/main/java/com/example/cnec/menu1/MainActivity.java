301a4d3a6cd43064ea411a896d24cf0df621558b
evertondewes <everton.dewes@gmail.com>
2018-09-03 21:30:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cnec/menu1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.cnec.menu1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater m = getMenuInflater();
+        m.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.iSalvar:
+                Toast.makeText(getApplicationContext(), "salvar",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.iExcluir:
+                Toast.makeText(getApplicationContext(), "Excluir",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return true;
+    }
+
+
+}
