c0a979663e00486c2c28da5043d6ce389ad32fc4
evertondewes <everton.dewes@gmail.com>
2018-09-03 21:58:14 -0300

    adicionado o menu de contexto

--- app/src/main/java/com/example/cnec/menu1/MainActivity.java
+++ app/src/main/java/com/example/cnec/menu1/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.cnec.menu1;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout lay = findViewById(R.id.tela_principal);
+
+        registerForContextMenu(lay);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.iSalvar:
+                Toast.makeText(getApplicationContext(), "Salvar",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.iExcluir:
+                Toast.makeText(getApplicationContext(), "Excluir",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
     }
 
     @Override
